3ad9a0c0e197cd8b6e3be3739b7f54c7d4b736fe
Kevin Purdy <kevin@ridewithgps.com>
2014-12-09 18:31:01 -0800

    Even more refactoring.

---
+++ FileSpider.py
@@ -0,0 +1,58 @@
+import os
+import re
+
+class FileSpider:
+    def __init__(self, config, matcher):
+        self.matcher = matcher
+        self.config = config
+
+    def build_file_list(self, matchSpec):
+        glob_matcher = re.compile(".*" + matchSpec + ".*", re.I)
+        file_list = {}
+        for root, subs, files in os.walk(self.config.get('general', 'basedir')):
+            for each in subs:
+                if glob_matcher.match(each):
+                    (d, fn) = self.build_tuple(self.matcher, each)
+                    file_list[d] = (fn, '')
+
+            for each in files:
+                if glob_matcher.match(each):
+                    (d1, fn) = self.build_tuple(self.matcher, each)
+                    (d2, dirn) = self.build_tuple(self.matcher, root)
+                    d = d1 if len(d1) > len(d2) else d2
+
+                    file_list[d] = (dirn, fn)
+
+        return file_list
+
+    def build_tuple(self, filename):
+        match = self.matcher.search(filename)
+        if match:
+            a = ".".join(match.groups()[-3:])
+        else:
+            a = ""
+        return a, filename
+
+    @staticmethod
+    def filter_missing(have, found):
+        possible = []
+        for each in found.keys():
+            if each not in have:
+                possible.append(found[each])
+        return possible
+
+    @staticmethod
+    def load_ignored():
+        try:
+            fh = open(os.path.expanduser("~/.pspid_ignored"))
+            ignored = pickle.load(fh)
+            fh.close
+        except:
+            ignored = []
+        return ignored
+
+    @staticmethod
+    def save_ignored(ignored):
+        fh = open(os.path.expanduser("~/.pspid_ignored"), "w")
+        pickle.dump(ignored, fh)
+        fh.close()
--- pspid.py
+++ pspid.py
@@ -1,67 +1,11 @@
 #!/usr/bin/env python
 
-import os
 import re
-import pickle
 import ConfigParser
 import sys
 import SabAPI
 import NabAPI
-
-class PSpid:
-    def __init__(self, matcher):
-        self.matcher = matcher
-
-    def build_file_list(self, matchSpec):
-        glob_matcher = re.compile(".*" + matchSpec + ".*", re.I)
-        file_list = {}
-        for root, subs, files in os.walk(config.get('general', 'basedir')):
-            for each in subs:
-                if glob_matcher.match(each):
-                    (d, fn) = self.build_tuple(self.matcher, each)
-                    file_list[d] = (fn, '')
-
-            for each in files:
-                if glob_matcher.match(each):
-                    (d1, fn) = self.build_tuple(self.matcher, each)
-                    (d2, dirn) = self.build_tuple(self.matcher, root)
-                    d = d1 if len(d1) > len(d2) else d2
-
-                    file_list[d] = (dirn, fn)
-
-        return file_list
-
-    def build_tuple(self, matcher, filename):
-        match = matcher.search(filename)
-        if match:
-            a = ".".join(match.groups()[-3:])
-        else:
-            a = ""
-        return a, filename
-
-    @staticmethod
-    def filter_missing(have, found):
-        possible = []
-        for each in found.keys():
-            if each not in have:
-                possible.append(found[each])
-        return possible
-
-    @staticmethod
-    def load_ignored():
-        try:
-            fh = open(os.path.expanduser("~/.pspid_ignored"))
-            ignored = pickle.load(fh)
-            fh.close
-        except:
-            ignored = []
-        return ignored
-
-    @staticmethod
-    def save_ignored(ignored):
-        fh = open(os.path.expanduser("~/.pspid_ignored"), "w")
-        pickle.dump(ignored, fh)
-        fh.close()
+import FileSpider
 
 if __name__ == "__main__":
 
@@ -70,7 +14,7 @@ if __name__ == "__main__":
     config = ConfigParser.ConfigParser()
     config.read('pspid.conf')
 
-    pspid = PSpid(dateMatcher)
+    filespider = FileSpider.FileSpider(config, dateMatcher)
     sabapi = SabAPI.SabAPI(config)
     nabapi = NabAPI.NabAPI(config, dateMatcher)
 
@@ -80,7 +24,7 @@ if __name__ == "__main__":
     if len(sys.argv) > 2:
         search_crit += " " + sys.argv[2]
 
-    have = pspid.build_file_list(file_crit)
+    have = filespider.build_file_list(file_crit)
 
     print "Have {0} files matching on disk".format(len(have))
 
@@ -88,9 +32,9 @@ if __name__ == "__main__":
 
     print "Found {0} results on the indexer".format(len(found))
 
-    possible = pspid.filter_missing(have, found)
+    possible = filespider.filter_missing(have, found)
 
-    ignored = pspid.load_ignored()
+    ignored = filespider.load_ignored()
 
     for each in possible:
         if each.guid() not in ignored:
@@ -105,4 +49,4 @@ if __name__ == "__main__":
             else:
                 ignored.append(each['guid'])
 
-    pspid.save_ignored(ignored)
+    filespider.save_ignored(ignored)

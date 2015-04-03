2be2f19593d2f88bd0a7e8a3cb746639d4bdb93b
Kevin Purdy <purdyk@gmail.com>
2015-04-02 18:19:56 -0700

    File system spider should follow links.

--- FCSpider.py
+++ FCSpider.py
@@ -12,7 +12,7 @@ class FileSpider:
     def build_file_list(self, matchSpec):
         glob_matcher = re.compile(".*" + matchSpec + ".*", re.I)
         file_list = {}
-        for root, subs, files in os.walk(self.config.get('general', 'basedir')):
+        for root, subs, files in os.walk(self.config.get('general', 'basedir'), None, True):
             for each in subs:
                 if glob_matcher.match(each):
                     (d, fn) = self.build_tuple(each)
